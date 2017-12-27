582daeb5c3af1e1f47f11ca6153a987a4b679fb1
Gustavo Adolfo Ossio Jiménez <gustavo.ossio@gmx.com>
2017-12-27 18:01:47 -0500

    Complete

---
+++ ApiService/ApiServiceRest.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiService
+{
+    public class ApiServiceRest
+    {
+        public async Task<Resource> GetList<T>(string urlBase, string controller)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var response = await client.GetAsync(controller);
+                var resultAsync = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Resource
+                    {
+                        IsSuccesful = response.IsSuccessStatusCode,
+                        Message = $"{response.ReasonPhrase} {response.RequestMessage}",
+                        Result = null
+                    };
+                }
+
+                var listAsync = JsonConvert.DeserializeObject<List<T>>(resultAsync);
+
+                return new Resource
+                {
+                    IsSuccesful = response.IsSuccessStatusCode,
+                    Result = listAsync,
+                    Message = $"{response.ReasonPhrase} {response.RequestMessage}"
+                };
+            }
+            catch (Exception e)
+            {
+                return new Resource
+                {
+                    Message = $"Error: {e.Message}",
+                    IsSuccesful = false,
+                    Result = null
+                };
+            }
+
+        }
+    }
+}
---
+++ ApiService/Resource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiService
+{
+    public class Resource
+    {
+        public string Message { get; set; }
+        public object Result { get; set; }
+        public bool IsSuccesful { get; set; }
+    }
+}
